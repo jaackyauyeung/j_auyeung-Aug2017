de9749b917aa707e1bd8727dcd05d716cff5c64a
jaackyauyeung <19auyeungj94@millshighsch.org>
2017-08-18 08:55:03 -0700

    My firs
    hhh

---
+++ Hello_World/src/Hello_World.java
@@ -0,0 +1,13 @@
+/**
+ * The class Hello_World shows Jacky Au-Yeung saying hello to the world.
+ * 
+ * @author (Jacky Au-Yeung)
+ * Version (1.0)
+ */
+public class Hello_World {
+public static void main(String[] args)
+{
+	System.out.println("Hello World! From Jacky Au-Yeung");
+	//Display the string
+}
+}
