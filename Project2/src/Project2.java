5bab6b841995907a355c8d052e35eab9b543a5af
jaackyauyeung <19auyeungj94@millshighsch.org>
2017-08-22 09:28:16 -0700

    This is my second project.

---
+++ Project2/src/Project2.java
@@ -0,0 +1,16 @@
+/*
+ * This program is my second java project. It will compare print and println statements.
+ * @author (Jacky Au-Yeung)
+ * Version (1.0)
+ * August 22, 2017
+ */
+public class Project2 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello this is my project 2!");
+		System.out.println();
+        System.out.print("In this project I will compare print and println statements.");
+	}
+
+}
